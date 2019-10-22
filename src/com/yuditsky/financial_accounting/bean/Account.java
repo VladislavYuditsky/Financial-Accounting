3ccf960a19fbd4768963aa49bfbfc9b14b28de6d
JFreshek <48886204+JFreshek@users.noreply.github.com>
2019-10-23 00:08:46 +0300

    Add Account bean

---
+++ src/com/yuditsky/financial_accounting/bean/Account.java
@@ -0,0 +1,39 @@
+package com.yuditsky.financial_accounting.bean;
+
+import java.util.Objects;
+
+public class Account {
+    private double amount;
+
+    public Account(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Double.compare(account.amount, amount) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "amount=" + amount +
+                '}';
+    }
+}
