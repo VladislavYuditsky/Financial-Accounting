30a7a638d586c54c9a92efc6442e3cb15d6f8d13
JFreshek <48886204+JFreshek@users.noreply.github.com>
2019-10-26 20:43:02 +0300

    readTransactions implemented

--- src/com/yuditsky/financial_accounting/service/impl/TransactionServiceImpl.java
+++ src/com/yuditsky/financial_accounting/service/impl/TransactionServiceImpl.java
@@ -1,12 +1,27 @@
 package com.yuditsky.financial_accounting.service.impl;
 
+import com.yuditsky.financial_accounting.dao.DAOException;
+import com.yuditsky.financial_accounting.dao.DAOFactory;
+import com.yuditsky.financial_accounting.dao.TransactionDAO;
 import com.yuditsky.financial_accounting.service.ServiceException;
 import com.yuditsky.financial_accounting.service.TransactionService;
 
 public class TransactionServiceImpl implements TransactionService {
     @Override
-    public boolean readTransactions() throws ServiceException {
-        return false;
+    public String readTransactions() throws ServiceException {
+
+        String transactions;
+
+        try {
+            DAOFactory daoObjectFactory = DAOFactory.getInstance();
+            TransactionDAO transactionDAO = daoObjectFactory.getTransactionDAO();
+
+            transactions = transactionDAO.readTransactions();
+        } catch (DAOException e) {
+            throw new ServiceException(e);
+        }
+
+        return transactions;
     }
 
     @Override
