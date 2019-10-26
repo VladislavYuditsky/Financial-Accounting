647853aa5112e9140d016a8e8ca734c95350865a
JFreshek <48886204+JFreshek@users.noreply.github.com>
2019-10-26 20:46:13 +0300

    implemented controller functionality

--- src/com/yuditsky/financial_accounting/controller/Controller.java
+++ src/com/yuditsky/financial_accounting/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.yuditsky.financial_accounting.controller;
 
+import com.yuditsky.financial_accounting.bean.User;
 import com.yuditsky.financial_accounting.controller.command.Command;
 
 public class Controller {
@@ -8,15 +9,35 @@ public class Controller {
 
     private final char paramDelimiter = ' ';
 
+    private User user = new User();
+
     public String executeTask(String request) {
         String commandName;
         Command executionCommand;
+        String response = null;
 
-        commandName = request.substring(0, request.indexOf(paramDelimiter));
-        executionCommand = provider.getCommand(commandName);
+        try {
+            commandName = request.substring(0, request.indexOf(paramDelimiter));
 
-        String response;
-        response = executionCommand.execute(request);
+            if (commandName.equals(CommandName.SIGN_IN.toString().toLowerCase()) && user.isAuthorized()) {
+                return "You are already logged in";
+            }
+
+            if (!commandName.equals(CommandName.SIGN_IN.toString().toLowerCase()) && !user.isAuthorized()) {
+                return "Log in";
+            }
+
+            executionCommand = provider.getCommand(commandName);
+
+
+            response = executionCommand.execute(request);
+
+            if (commandName.equals(CommandName.SIGN_IN.toString().toLowerCase()) && response.equals("Welcome")) {
+                user.setAuthorized(true);
+            }
+        }catch(StringIndexOutOfBoundsException e){
+            response = executeTask(request + " ");
+        }
 
         return response;
     }
