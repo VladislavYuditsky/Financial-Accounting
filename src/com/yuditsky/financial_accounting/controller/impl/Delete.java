675ec8043b2c6a2b9a8581e5b8ceff5bc144f6e8
JFreshek <48886204+JFreshek@users.noreply.github.com>
2019-10-27 13:52:19 +0300

    Delete command implemented

--- src/com/yuditsky/financial_accounting/controller/impl/Delete.java
+++ src/com/yuditsky/financial_accounting/controller/impl/Delete.java
@@ -1,10 +1,38 @@
 package com.yuditsky.financial_accounting.controller.impl;
 
 import com.yuditsky.financial_accounting.controller.command.Command;
+import com.yuditsky.financial_accounting.service.ServiceException;
+import com.yuditsky.financial_accounting.service.ServiceFactory;
+import com.yuditsky.financial_accounting.service.TransactionService;
 
 public class Delete implements Command {
+
+    private final char paramDelimiter = ' ';
+
     @Override
     public String execute(String request) {
-        return null;
+        String response;
+
+        try {
+            request = request.substring(request.indexOf(paramDelimiter) + 1);
+
+            int id = Integer.parseInt(request);
+
+            ServiceFactory serviceFactory = ServiceFactory.getInstance();
+            TransactionService transactionService = serviceFactory.getTransactionService();
+
+            if(transactionService.delete(id)){
+                response = "Deleted";
+            } else {
+                response = "No transaction with given id";
+            }
+
+        } catch (ServiceException e) {
+            response = "Error during deletion";
+        } catch (StringIndexOutOfBoundsException | IllegalArgumentException e) {
+            response = "Invalid command arguments";
+        }
+
+        return response;
     }
 }
